cb9d9037c006c972564efb7811f8a9b61d48411c
ITWILL <ITWILL@DESKTOP-ITMLQU7>
2022-10-17 12:04:26 +0900

    .

---
+++ lab01_intro/src/edu/java/intro2/AppMain2.java
@@ -0,0 +1,28 @@
+package edu.java.intro2;
+
+public class AppMain2 {
+
+	public static void main(String[] args) {
+		System.out.println("자바 프로그래밍 실행 방법");
+		System.out.println("Menu -> Run -> Run");
+		System.out.println("Ctrl+F11");
+		// 문자열(String): 문자들의 집합(Sequence). 큰따옴표("") 사용. 작은 따옴표 불가!!! <--> SQL에서는 작은 따옴표(''), 별명을 정할 때만 큰 따옴표("").
+		
+		// Ctrl+Space: 코드 추천 및 자동 완성.
+		
+		System.out.println("문자열" + "합치기"); // +: 문자열을 이어주는 연산자
+		System.out.println(1 + 2); // +:  덧셈 연산자
+		System.out.println("1" + "2");
+		System.out.println("1 + 2 = 3");
+		System.out.println("1 + 2 = " + (1 + 2));
+		
+		System.out.print("프린트");
+		System.out.print("Print");
+		
+		
+	
+	}
+
+}
+
+
